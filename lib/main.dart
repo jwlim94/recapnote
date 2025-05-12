954edd106335f6785dbba0f7ca0e9656541e950a
Jongwoo Lim <limjongwoo1994@gmail.com>
2025-05-12 11:25:47 -0600

    create env.dart and apply OpenAI env

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
+import 'package:dart_openai/dart_openai.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:recapnote/env/env.dart';
 import 'package:recapnote/firebase_options.dart';
 import 'package:recapnote/src/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Set API Keys
+  OpenAI.apiKey = Env.openAIAPIKey;
 
   // Initialize Firebase
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
