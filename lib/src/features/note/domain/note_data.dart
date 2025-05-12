0baaec752c03ef9cc6ac1a9d4373d60ff30fc837
Jongwoo Lim <limjongwoo1994@gmail.com>
2025-05-12 13:42:23 -0600

    create note_data

---
+++ lib/src/features/note/domain/note_data.dart
@@ -0,0 +1,8 @@
+class NoteData {
+  NoteData({this.id, this.question, this.answer, this.title});
+
+  final String? id;
+  final String? question;
+  final String? answer;
+  final String? title;
+}
